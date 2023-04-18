148a34aad7f2dafbfdd9d80bce99ae143007acbf
Daniel Negoita <danielnicolae.negoita.2160@lacetania.cat>
2023-04-18 15:26:32 +0200

    pagination

--- frontend/src/app/components/pagination/pagination.component.ts
+++ frontend/src/app/components/pagination/pagination.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
@@ -8,13 +9,23 @@ import { Component, Input, Output } from '@angular/core';
 export class PaginationComponent {
   @Input('current_page') current_page: number;
   @Input('last_page') last_page: number;
-  @Output('changePage') changePage: Function;
 
-  constructor() {
+  @Output('changePage') changePage= new EventEmitter<number>();
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
     this.current_page = 1;
     this.last_page = 1;
-    this.changePage = () => {
-      console.log("hola")
-    };
+  }
+
+  onChangePage(value: number){
+    this.changePage.emit(value);
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { page: value },
+      queryParamsHandling: 'merge',
+    });
   }
 }
